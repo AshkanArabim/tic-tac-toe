9c38a5280de76b25b2e975996757447440c83105
AshkanArabim <ashkan.arabim@gmail.com>
2022-08-02 13:27:47 -0600

    add win logic

--- script.js
+++ script.js
@@ -18,6 +18,44 @@ const game = (function () {
     const gameboard = (function () {
         let _board = [null,null,null,null,null,null,null,null,null];
 
+        function _hasWon() {
+            function ThreeEq(a,b,c) {
+                if (
+                    a !== null &&
+                    (a === b &&
+                    b === c &&
+                    c === a)
+                ) {
+                    return true;
+                } else {
+                    return false;
+                };
+            }
+
+            if(
+                ThreeEq(_board[0], _board[1], _board[2]) ||
+                ThreeEq(_board[3], _board[4], _board[5]) ||
+                ThreeEq(_board[6], _board[7], _board[8]) ||
+                ThreeEq(_board[0], _board[3], _board[6]) ||
+                ThreeEq(_board[1], _board[4], _board[5]) ||
+                ThreeEq(_board[2], _board[5], _board[8]) ||
+                ThreeEq(_board[0], _board[4], _board[8]) ||
+                ThreeEq(_board[2], _board[4], _board[6])
+            ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function _boardFull() {
+            if (!(_board.includes(null))) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
         function restart() {
             for (let cellIndex in cells) {
                 cells[cellIndex].classList.remove('x');
@@ -49,23 +87,12 @@ const game = (function () {
                 _board[clickedCell] = sign;
             }
 
-            console.log(_board)
-
             switchTurn()
             _render()
-            
-            if (
-                _board[0] === _board[1] === _board[2] ||
-                _board[3] === _board[4] === _board[5] ||
-                _board[6] === _board[7] === _board[8] ||
-                _board[0] === _board[3] === _board[6] ||
-                _board[1] === _board[4] === _board[5] ||
-                _board[2] === _board[5] === _board[8] ||
-                _board[0] === _board[4] === _board[8] ||
-                _board[2] === _board[4] === _board[6]
-            ) {
+
+            if (_hasWon()) {
                 win(sign);
-            } else if (!(_board.includes(null))) {
+            } else if (_boardFull()) {
                 alert('board full')
             }
         }
